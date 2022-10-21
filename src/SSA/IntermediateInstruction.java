3b86b3fbd1e639b4767b8f44503869ca6355c47f
Max <maxsmith271346@tamu.edu>
2022-10-21 13:08:44 -0500

    Added SSAOperator enum

--- src/SSA/IntermediateInstruction.java
+++ src/SSA/IntermediateInstruction.java
@@ -8,11 +8,62 @@ public class IntermediateInstruction {
     // should we make Integer Literal & Float Literal & Bool Literal extend an Operator interface? 
     // should we make a LineNumber class that also extends Operator interface? 
 
-    private String operator; 
+    public enum SSAOperator {
+        NEG("NEG"),
+        ADD("ADD"),
+        SUB("SUB"),
+        MUL("MUL"),
+        DIV("DIV"),
+        MOD("MOD"),
+        POW("POW"),
+        CMP("CMP"),
+        NOT("NOT"),
+        OR("OR"),
+        AND("AND"),
+
+        ADDA("ADDA"),
+        LOAD("LOAD"),
+        STORE("STORE"),
+        PHI("PHI"),
+        END("END"),
+        BRA("BRA"),
+        BNE("BNE"),
+        BEQ("BEQ"),
+        BLE("BLE"),
+        BLT("BLT"),
+        BGE("BGE"),
+        BGT("BGT"),
+
+        READ("READ"),
+        WRITE("WRITE"),
+        READ_B("READ_B"),
+        WRITE_B("WRITE_B"),
+        READ_F("READ_F"),
+        WRITE_F("WRITE_F"),
+        WRITE_NL("WRITE_NL");
+
+    
+        private String opString;
+
+        SSAOperator () {
+            opString = "";
+        }
+        
+        SSAOperator (String opString) {
+            this.opString = opString;
+        }
+
+        public String getOpString () {
+            return this.opString;
+        }
+
+    }
+
+    private SSAOperator operator; 
     private Operand operand_one; 
     private Operand operand_two; 
 
-    public IntermediateInstruction(String operator, Operand operand_one, Operand operand_two){
+    public IntermediateInstruction(SSAOperator operator, Operand operand_one, Operand operand_two){
         this.operator = operator; 
         this.operand_one = operand_one; 
         this.operand_two = operand_two;
